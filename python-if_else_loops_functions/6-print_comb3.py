1eb6c2427e2ac6ef98facf0faefc1b485bff50c3
OrpheliePerrine <m.perrine@alustudent.com>
2025-08-18 05:26:26 -0700

    combination

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for i in range(0, 9):
-    for j in range(0, 9):
-        if i != j:
-            print("{:02}".format(ij), end=", ")
+for i in range(10):
+    for j in range(i + 1, 10):
+        print(f"{i}{j}", end=", ")
