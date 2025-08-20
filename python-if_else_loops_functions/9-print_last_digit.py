60388b45acee43dff7831db3f7328c0eb3c81249
OrpheliePerrine <m.perrine@alustudent.com>
2025-08-20 02:12:51 -0700

    last digit

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    for n in number:
-        n = str(n)[-1]
-    print("{}".format(n))
+    last_digit = abs(number) % 10
+    print(last_digit, end="")
+    return last_digit
