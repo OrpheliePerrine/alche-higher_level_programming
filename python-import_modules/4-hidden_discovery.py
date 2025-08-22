1a2af524c620e959a180b58949639198b09727b9
OrpheliePerrine <m.perrine@alustudent.com>
2025-08-22 02:14:39 -0700

    hidden

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,7 @@
 import hidden_4
 
 if __name__ == "__main__":
-    name = dir(hidden_4)
+    names = dir(hidden_4)   # define names here
     for name in sorted(names):
         if not name.startswith("__"):
             print(name)
