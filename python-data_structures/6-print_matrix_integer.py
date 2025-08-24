52b4763ea9e26f2931888d4bbd0fa2b1845f462a
OrpheliePerrine <m.perrine@alustudent.com>
2025-08-24 11:50:21 -0700

    matrix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i, num in enumerate(row):
             if i != 0:
-                print(" ", end="")
-            print("{:d}".format(num), end="")
-        print()    
+                print(" ", end="")  # add a space between numbers
+            print("{:d}".format(num), end="")  # print integer without newline
+        print()  # new line after each row
+
