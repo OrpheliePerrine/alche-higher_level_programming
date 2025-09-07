ebfbb427c571ddb874fa89774d18a62c907d022e
OrpheliePerrine <m.perrine@alustudent.com>
2025-09-07 13:28:30 -0700

    square

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -15,4 +15,6 @@ class Square(Rectangle):
 
     def __str__(self):
         """Return string representation of the square."""
-        return "[Square] {}/{}".format(self._Rectangle__width, self._Rectangle__height)
+        return "[Square] {}/{}".format(
+            self._Rectangle__width, self._Rectangle__height
+        )
