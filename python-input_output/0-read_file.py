ddbefb74c49e75c9ef127c4629641d5158991f50
OrpheliePerrine <m.perrine@alustudent.com>
2025-09-27 00:46:57 -0700

    adding documentation

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""Module that contains the read_file function."""
+
+
 def read_file(filename=""):
-    """Reads a text file (UTF8) and prints its content to stdout."""
+    """Reads a text file (UTF8) and prints it to stdout.
+
+    Args:
+        filename (str): The path to the file to read.
+    """
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
-
