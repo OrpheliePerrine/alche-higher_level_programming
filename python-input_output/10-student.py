0a576e5a6153812ce730b82c1560680d504c9ecf
OrpheliePerrine <m.perrine@alustudent.com>
2025-09-29 23:36:37 -0700

    student class

---
+++ python-input_output/10-student.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+This module defines a Student class with public attributes
+and a method to return a dictionary representation suitable
+for JSON serialization, optionally filtered by a list of
+attribute names.
+"""
+
+
+class Student:
+    """
+    Represents a student with first name, last name, and age.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a Student instance.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Returns a dictionary representation of the Student instance.
+
+        If attrs is a list of strings, only attributes with names
+        in this list will be included. Otherwise, all attributes
+        are included.
+
+        Args:
+            attrs (list, optional): List of attribute names to include.
+
+        Returns:
+            dict: Dictionary representation of the instance.
+        """
+        if isinstance(attrs, list) and all(isinstance(a, str) for a in attrs):
+            return {key: value for key, value in self.__dict__.items() if key in attrs}
+        return self.__dict__.copy()
