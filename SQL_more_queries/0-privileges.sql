c183b7041dda72444f09eec445dd0f5a5a566fd7
OrpheliePerrine <m.perrine@alustudent.com>
2025-09-30 00:50:15 -0700

    editing

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,9 @@
--- 0-privileges.sql
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- 1-create_user.sql
+
+-- Create user_0d_1 with ALL privileges (root-like)
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- Create user_0d_2 with limited privileges on a specific database
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT, INSERT ON user_2_db.* TO 'user_0d_2'@'localhost';
